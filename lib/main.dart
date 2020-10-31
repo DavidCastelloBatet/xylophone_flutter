1eb4b2fb9f3c8d70e2d9196fd3045d99bd870a54
David Castello <davidcastellobatetgmail.com>
2020-10-31 20:05:08 +0100

    versio final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -7,10 +8,40 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Xilòfon',
+            style: TextStyle(fontSize: 30.0, letterSpacing: 3.0),
+          ),
+          backgroundColor: Colors.black,
+        ),
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(child: playSound(num: 1, color: Colors.red)),
+              Expanded(child: playSound(num: 2, color: Colors.orange)),
+              Expanded(child: playSound(num: 3, color: Colors.yellow)),
+              Expanded(child: playSound(num: 4, color: Colors.green)),
+              Expanded(child: playSound(num: 5, color: Colors.teal)),
+              Expanded(child: playSound(num: 6, color: Colors.blue)),
+              Expanded(child: playSound(num: 7, color: Colors.purple)),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+Widget playSound({num, color}) {
+  return FlatButton(
+    color: color,
+    onPressed: () {
+      final player = AudioCache();
+      player.play('note$num.wav');
+    },
+    child: null,
+  );
 }
